12659233e2da47e069c0a8cb0d9a6a5186e3aa20
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-06-13 18:09:23 +0200

    Initial Commit

---
+++ MiniDJ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniDJ
+//
+//  Created by Yuri Petrosyan on 13/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniDJ/MiniDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniDJApp.swift
+//  MiniDJ
+//
+//  Created by Yuri Petrosyan on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiniDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
