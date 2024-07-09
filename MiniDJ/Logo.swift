eda9d625d95e9b3b967dc1f06bd460a0a1641388
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-07-09 23:35:43 +0200

    UI changes

---
+++ MiniDJ/Logo.swift
@@ -0,0 +1,18 @@
+//
+//  Logo .swift
+//  Controllers
+//
+//  Created by Yuri Petrosyan on 05/07/2024.
+//
+
+import SwiftUI
+
+struct Logo_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logo_()
+}
