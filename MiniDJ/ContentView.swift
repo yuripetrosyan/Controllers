fa44d1f593d6cc4b35efd47892f52598dc481ab6
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-07-12 21:00:27 +0200

    Bug Fix

--- MiniDJ/ContentView.swift
+++ MiniDJ/ContentView.swift
@@ -169,7 +169,7 @@ struct ContentView: View {
                 
                 .ignoresSafeArea()
             }.onAppear{
-                viewModel.loadMockMP3()
+                //viewModel.loadMockMP3()
             }
         }
     }
